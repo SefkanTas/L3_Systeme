67e0aa48ce90240bd699cfc0b60125dadbc0cc16
Sefkan <sefkan.etud@gmail.com>
2020-03-23 14:04:07 +0100

    le projet fonctionne !

--- main.c
+++ main.c
@@ -115,7 +115,7 @@ int is_separator(char c){
 void fork_check(int fork_res){
     if(fork_res == -1){
         printf("Erreur de fork\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -142,7 +142,7 @@ FILE * get_file(char *path){
     FILE *file = fopen(path, "r");
     if (file == NULL){
         printf("Erreur de fichier\n");
-        exit(2);
+        exit(EXIT_FAILURE);
     }
     return file;
 }
@@ -298,13 +298,108 @@ void display_element_array(element_array ea){
     }
 }
 
+
+
+int is_type_valid(const char *type){
+    int is_valid = 0;
+    if(strlen(type) != 1){
+        return is_valid;
+    }
+
+    char valid_types[] = "cws";
+    int i = 0;
+    while(i < strlen(valid_types) && !is_valid){
+        if(type[0] == valid_types[i]){
+            is_valid = 1;
+        }
+        i++;
+    }
+
+    return is_valid;
+}
+
+
+int is_number(const char *nb_enf){
+    int is_number = 1;
+    int i = 0;
+    while(i < strlen(nb_enf) && is_number){
+        if(!isdigit(nb_enf[i])){
+            is_number = 0;
+        }
+        i++;
+    }
+    return is_number;
+}
+
 int main(int argc, char const *argv[]) {
+
+    const int NB_ENFANT_MIN = 2;
+    const int NB_ENFANT_MAX = 8;
 
     int nb_enfants = 2;
     int nb_lg = 2;
     int line_size = 1024;
     char *files_list[] = {"data/text.txt", "data/extra_mini_lorem.txt"};
     int nb_files = 2;
+    FileManager fm;
+
+    if(argc < 5){
+        printf("Nombre de parametre inccorecte.\n");
+        printf("%s type_comptage nb_enfants nb_lg liste_fichiers\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if(!is_type_valid(argv[1])){
+        printf("Le type de comptage est invalide (c, w, s).\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(!is_number(argv[2])){
+        printf("Le nombre d'enfants est invalide.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(!is_number(argv[3])){
+        printf("Le nombre de lignes est invalide.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char type_comptage = argv[1][0];
+    nb_enfants = atoi(argv[2]);
+
+    if(nb_enfants == 0){
+        nb_enfants = sysconf(_SC_NPROCESSORS_ONLN);
+    }
+    if(nb_enfants < NB_ENFANT_MIN){
+        nb_enfants = NB_ENFANT_MIN;
+    }
+    else if (nb_enfants > NB_ENFANT_MAX) {
+        nb_enfants = NB_ENFANT_MAX;
+    }
+
+    nb_lg = atoi(argv[3]);
+
+    char *filess[argc - 4];
+    int file_error = 0;
+    for(int i = 4; i < argc; i++){
+        if(access(argv[i], R_OK) == -1){
+            printf("Le fichier \"%s\" n'existe pas.\n", argv[i]);
+            file_error = 1;
+        }
+    }
+
+    if(file_error){
+        exit(EXIT_FAILURE);
+    }
+
+    fm.nb_files = argc - 4;
+    fm.files_path = malloc(sizeof(char) * nb_files);
+    char *oui[nb_files];
+    for(int i = 4; i < argc; i++){
+        fm.files_path[i-4] = malloc(sizeof(char) * strlen(argv[i]) + 1);
+        strcpy(fm.files_path[i-4], argv[i]);
+    }
+
 
     int worker_id;
     int pid_producteur = -1;
@@ -344,10 +439,8 @@ int main(int argc, char const *argv[]) {
             close(pipes_ptow[i][0]);
         }
 
-        FileManager fm;
         fm.index = 0;
         fm.last_used_index = -1;
-        fm.files_path = files_list;
         fm.nb_files = 2;
         fm.is_done = 0;
 
@@ -420,11 +513,21 @@ int main(int argc, char const *argv[]) {
         }
 
         char data[line_size * nb_lg];
+        element_array data_block;
 
         while (read(pipes_ptow[worker_id][0], data, sizeof(data)) > 0) {
-            printf("MON WORKER_ID %d\n", worker_id);
-
-            send_data_wtoc(separator_count(data), pipe_wtoc);
+            switch (type_comptage) {
+                case 'c':
+                    data_block = char_count(data);
+                    break;
+                case 'w':
+                    data_block = word_count(data);
+                    break;
+                case 's':
+                    data_block = separator_count(data);
+                    break;
+            }
+            send_data_wtoc(data_block, pipe_wtoc);
             write(pipe_wtop[1], &worker_id, sizeof(int));
         }
         printf("end worker : %d\n", worker_id);
